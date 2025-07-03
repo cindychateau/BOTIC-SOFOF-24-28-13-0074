82f474b451d2b11543aabd4d204558bf8f414fa4
Cynthia Castillo <ca.castilloe@gmail.com>
2025-07-03 17:45:14 -0600

    Retos de Numerales

--- Módulo 3/tipos_datos.py
+++ Módulo 3/tipos_datos.py
@@ -1,3 +1,4 @@
+import random
 #BOOLEANS
 mayor_edad = True
 tiene_bigote = False
@@ -34,3 +35,26 @@ print(numero_maximo)
 
 numero_minimo = min(2, 3, 1) #Recibe n cantidad de números y regresa el menor
 print(numero_minimo)
+
+num_aleatorio = random.randint(5, 10) #Número entero aleatorio entre 5 y 10
+decimal_aleatorio = random.uniform(-10, 10) #Número decimal aleatorio entre -10 y 10
+print(num_aleatorio)
+print(decimal_aleatorio)
+
+'''
+Genera 5 números enteros aleatorios enteros entre 10 y 10,000 usando la librería random.
+Para cada número:
+Muestra su valor y su tipo de dato usando la función type().
+Muestra cuántos dígitos tiene utilizando len().*
+Muestra el resultado con un mensaje descriptivo.
+'''
+
+'''
+Genera 3 números aleatorios con decimales entre -100 y 100 usando random.uniform().
+Para cada número realiza lo siguiente:
+Muestra su valor original y su tipo de dato (type()).
+Muestra su valor absoluto usando abs().
+Redondea el número a 2 decimales con round().
+Convierte el número redondeado a entero usando int().
+¿Qué diferencia hay entre round() e int()?
+'''
