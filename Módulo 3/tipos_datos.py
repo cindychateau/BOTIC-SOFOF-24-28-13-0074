68741b04bea3a0d961fa68f1d20ebee811d1535f
Cynthia Castillo <ca.castilloe@gmail.com>
2025-07-03 19:02:20 -0600

    Retos Numerales Adicionales

--- Módulo 3/tipos_datos.py
+++ Módulo 3/tipos_datos.py
@@ -1,4 +1,6 @@
 import random
+import math
+
 #BOOLEANS
 mayor_edad = True
 tiene_bigote = False
@@ -48,6 +50,18 @@ Muestra su valor y su tipo de dato usando la función type().
 Muestra cuántos dígitos tiene utilizando len().*
 Muestra el resultado con un mensaje descriptivo.
 '''
+num1 = random.randint(10,10000)
+num2 = random.randint(10,10000)
+num3 = random.randint(10,10000)
+num4 = random.randint(10,10000)
+num5 = random.randint(10,10000)
+
+print(f"El valor es:{num1}, su tipo es:{type(num1)}, y la cantidad de digitos es: {len(str(num1))}")
+print(f"El valor es:{num2}, su tipo es:{type(num2)}, y la cantidad de digitos es: {len(str(num2))}")
+print(f"El valor es:{num3}, su tipo es:{type(num3)}, y la cantidad de digitos es: {len(str(num3))}")
+print(f"El valor es:{num4}, su tipo es:{type(num4)}, y la cantidad de digitos es: {len(str(num4))}")
+print(f"El valor es:{num5}, su tipo es:{type(num5)}, y la cantidad de digitos es: {len(str(num5))}")
+
 
 '''
 Genera 3 números aleatorios con decimales entre -100 y 100 usando random.uniform().
@@ -58,3 +72,51 @@ Redondea el número a 2 decimales con round().
 Convierte el número redondeado a entero usando int().
 ¿Qué diferencia hay entre round() e int()?
 '''
+
+num1 = random.uniform(-100, 100)
+num2 = random.uniform(-100, 100)
+num3 = random.uniform(-100, 100)
+
+print(f"El valor es:{num1}, su tipo es:{type(num1)}, el absoluto es: {abs(num1)}, redondeado es: {round(num1, 2)} usando int: {int(num1)}")
+print(f"El valor es:{num2}, su tipo es:{type(num2)}, el absoluto es: {abs(num2)}, redondeado es: {round(num2, 2)} usando int: {int(num2)}")
+print(f"El valor es:{num3}, su tipo es:{type(num3)}, el absoluto es: {abs(num3)}, redondeado es: {round(num3, 2)} usando int: {int(num3)}")
+
+'''
+Pide al usuario que ingrese dos números (enteros o decimales).
+Para cada número, calcula y muestra:
+La raíz cuadrada (de su valor absoluto).
+Su valor elevado a la tercera potencia.
+Su valor absoluto.
+El redondeo hacia arriba (ceil) y hacia abajo (floor).
+Luego, muestra las operaciones entre ambos números:
+Suma, resta, multiplicación y división.
+Máximo y mínimo de los dos números.
+Potencia del primer número elevado al segundo (math.pow()).
+'''
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+print("--Para el primer número--")
+print(f"La raíz cuadrada (de su valor absoluto): {math.sqrt(abs(numero1))}")
+print(f"Su valor elevado a la tercera potencia: {math.pow(numero1, 3)}")
+print(f"Su valor absoluto: {math.fabs(numero1)}")
+print(f"Redondeo hacia arriba: {math.ceil(numero1)}, Redondeo hacia abajo: {math.floor(numero1)}")
+
+print("--Para el segundo número--")
+print(f"La raíz cuadrada (de su valor absoluto): {math.sqrt(abs(numero2))}")
+print(f"Su valor elevado a la tercera potencia: {math.pow(numero2, 3)}")
+print(f"Su valor absoluto: {math.fabs(numero2)}")
+print(f"Redondeo hacia arriba: {math.ceil(numero2)}, Redondeo hacia abajo: {math.floor(numero2)}")
+
+print(f"Suma de ambos números: {numero1 + numero2}")
+print(f"Resta de ambos números: {numero1 - numero2}")
+print(f"Multiplicación de ambos números: {numero1 * numero2}")
+
+if numero2 != 0:
+    print(f"División de ambos números: {numero1 / numero2}")
+else:
+    print("No puedo hacer la división.")
+
+print(f"El número máximo es: {max(numero1, numero2)}")
+print(f"El número minimo es: {min(numero1, numero2)}")
+print(f"Num1 elevado a la potencia de Num2 es: {math.pow(numero1, int(numero2))}")
