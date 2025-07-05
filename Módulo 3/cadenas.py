8e1cdf39de01d3f3b0a1e842ea221db1322f114e
Cynthia Castillo <ca.castilloe@gmail.com>
2025-07-04 19:02:10 -0600

    Retos Adicionales: Cadenas

--- Módulo 3/cadenas.py
+++ Módulo 3/cadenas.py
@@ -49,6 +49,20 @@ Usa la función random.choice() para seleccionar aleatoriamente entre dos posibl
 
 Muestra el mensaje, personalizando el lugar y número con los datos ingresados. Convierte el nombre a mayúsculas en el saludo.
 '''
+import random
+nombre = input("Escribe tu nombre:")
+numero = input("Escribe tu num favorito:")
+lugar = input("Escribe tu lugar favorito:")
+
+mensajes = [
+    f"Hoy es un buen día para ti en {lugar}. Confía en tu número {numero}.",
+    f"Tal vez hoy no sea el mejor día para visitar {lugar}. Evita pensar en el número {numero}."
+]
+
+print(f"¡Hola {nombre.upper()}! {random.choice(mensajes)}")
+
+
+
 
 '''
 Pide al usuario que escriba una frase.
@@ -68,3 +82,25 @@ Frase censurada: ¡Me encanta el rXck and rXll!
 Cantidad de caracteres: 29
 
 '''
+frase = input("Escribe una frase: ")
+letra = input("Escribe una letra: ")[:1] #Limita a que la cantidad de caracteres sea 1
+
+frase_censurada = frase.replace(letra.lower(), "X") #busca la letra en minúscula y la reemplaza por la X
+frase_censurada = frase_censurada.replace(letra.upper(), "X") #busca la letra en mayúscula y la reemplaza por la X
+print(f"Frase original: {frase}")
+print(f"Frase censurada: {frase_censurada}")
+print(f"Cantidad de caracteres: {len(frase)}")
+
+'''
+Pide al usuario que escriba una palabra secreta.
+Muestra la siguiente información usando métodos de strings:
+Cantidad de caracteres: con len().
+En mayúsculas: con .upper().
+En minúsculas: con .lower().
+¿Empieza con “a”? usando .startswith().
+¿Termina con “z”? usando .endswith().
+Cantidad de veces que aparece la letra “e”: usando .count().
+La palabra sin espacios adicionales: usando .strip().
+La palabra reemplazando las vocales “a” por “@”: usando .replace().
+'''
+
