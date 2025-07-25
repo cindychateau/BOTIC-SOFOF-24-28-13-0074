70858ac25416cf8787840470a7f046ff2ff6f47f
Cynthia Castillo <ca.castilloe@gmail.com>
2025-07-24 19:03:05 -0600

    Metodos estaticos

--- Módulo 4/Animal.py
+++ Módulo 4/Animal.py
@@ -25,10 +25,12 @@ class Animal:
     def presentarse(self): #self = miu
         #¡Hola yo soy Miu, un Gato, y tengo 7 año(s)!
         print(f"¡Hola yo soy {self.nombre}, un {self.tipo}, y tengo {self.edad} año(s)!")
+        print(f"Calidad de vida de animalito: {Animal.calidad_de_vida(self.energia, self.felicidad)}")
         return self #Permite encadenamiento de métodos
     
     def comer(self, comida): #self = miu, comida = "croquetas"
-        self.energia += 5 #miu.energia = 100 + 5 = 105
+        #self.energia += 5 #miu.energia = 100 + 5 = 105
+        self.energia = Animal.calcula_energia(self.energia, 5)
         self.felicidad += 5 #miu.felicidad = 100 + 5 = 105
         print(f"{self.nombre} está comiendo {comida}")
         return self
@@ -36,15 +38,18 @@ class Animal:
     def jugar(self, horas): #self = firulais, horas = 5
         #Firulais estuvo jugando por 5 horas
         print(f"{self.nombre} estuvo jugando por {horas} horas")
-        self.energia -= (horas * .5) #firulais.energia = 100 - (5*.5) = 97.5
+        #self.energia -= (horas * .5) #firulais.energia = 100 - (5*.5) = 97.5
+        self.energia = Animal.calcula_energia(self.energia, -(horas*.5))
         self.felicidad += (horas * .5) #firulais.felicidad = 100 + (5 * .5) = 102.5
         return self
     
     def dormir(self, horas): #self = miu, horas = 2
         if horas < 2:
-            self.energia += 1
+            #self.energia += 1
+            self.energia = Animal.calcula_energia(self.energia, 1)
         elif horas < 5:
-            self.energia += 15
+            #self.energia += 15
+            self.energia = Animal.calcula_energia(self.energia, 15)
         else:
             self.energia = 100
         print(f"{self.nombre} durmió {horas} horas")
@@ -57,3 +62,19 @@ class Animal:
         for mascota in cls.lista_animalitos: 
             #animal = miu
             print(f"Nombre: {mascota.nombre}. Tipo: {mascota.tipo}. Edad: {mascota.edad}")
+
+    #Método estático: acción/función que debo realizar para la clase SIN EMBARGO no pertenece ni mi instancia ni a mi clase. Objetivo de ayudar y organizar mi código
+    @staticmethod
+    def calcula_energia(energia_actual, cambio_energia): 
+        suma = energia_actual + cambio_energia
+        if suma <= 100:
+            return suma
+        else: 
+            return 100
+
+    @staticmethod
+    def calidad_de_vida(energia, felicidad):
+        if energia >= 70 and felicidad >= 70:
+            return "BUENA"
+        else:
+            return "MEDIA"
