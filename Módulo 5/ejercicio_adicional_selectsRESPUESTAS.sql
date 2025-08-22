cf380faed1331b61c6e46a8a33a907c4256d1efa
Cynthia Castillo <ca.castilloe@gmail.com>
2025-08-22 16:58:44 -0600

    Update y Delete

--- Módulo 5/ejercicio_adicional_selectsRESPUESTAS.sql
+++ Módulo 5/ejercicio_adicional_selectsRESPUESTAS.sql
@@ -15,12 +15,29 @@ SELECT titulo AS cancion, nombre AS artista, genero FROM canciones
 JOIN artistas ON canciones.artista_id = artistas.artista_id
 ORDER BY artista DESC;
 
--- Consulta 6: Muestra la promedio de duración de todas las canciones
+-- Consulta 6: Muestra el promedio de duración de todas las canciones
+SELECT AVG(duracion) AS promedio_segundos FROM canciones;
 
--- Consulta 7: Muestra la promedio de todas las canciones del género pop
+-- Consulta 7: Muestra el promedio de todas las canciones del género pop
+SELECT AVG(duracion) AS promedio_segundos FROM canciones WHERE genero = "pop";
 
 -- Consulta 8: Muestra las canciones en cada playlist, incluye el nombre del playlist, titulo de la canción y nombre del artista. Ordénalas en base al nombre de la playlist
+SELECT playlists.nombre AS nombre_playlist, titulo, artistas.nombre AS nombre_artista FROM playlists
+JOIN playlist_cancion ON playlists.playlist_id = playlist_cancion.playlist_id
+JOIN canciones ON playlist_cancion.cancion_id = canciones.cancion_id
+JOIN artistas ON canciones.artista_id = artistas.artista_id
+ORDER BY nombre_playlist;
 
 -- Consulta 9: Muestra la cantidadad de canciones por género. Muestra primero los géneros que tienen más canciones
+SELECT genero, COUNT(*) AS cantidad
+FROM canciones
+GROUP BY genero
+ORDER BY cantidad DESC;
 
 -- Consulta 10: Muestra al artista con más canciones en la DB
+SELECT artistas.artista_id, nombre, COUNT(*) AS cantidad
+FROM canciones
+JOIN artistas ON canciones.artista_id = artistas.artista_id
+GROUP BY artistas.artista_id
+ORDER BY cantidad DESC
+LIMIT 1;
