72401cdaf4b708da923061708688273d69963fb2
Cynthia Castillo <ca.castilloe@gmail.com>
2025-10-29 03:02:59 +0300

    UD de Bicicletas

--- Módulo 7/bikeshop_project/bicicletas/urls.py
+++ Módulo 7/bikeshop_project/bicicletas/urls.py
@@ -3,5 +3,8 @@ from . import views #Importo views, en base a x ruta muestra y vista
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('crear/', views.crear, name='crear'),
+    path('crear/', views.crear, name='crear'), #nuevo/nueva
+    path('actualizar/<int:id>/', views.actualizar, name='actualizar'), #editar
+    path('eliminar/<int:id>/', views.eliminar, name='eliminar'), #borrar
+    path('detalle/<int:id>/', views.detalle, name='detalle'), #vista
 ]
--- Módulo 7/bikeshop_project/bicicletas/views.py
+++ Módulo 7/bikeshop_project/bicicletas/views.py
@@ -13,5 +13,24 @@ def crear(request):
         if form.is_valid():
             form.save()
             return redirect('home')
-            
-    return render(request, 'crear.html', {"form": form})
+
+    return render(request, 'formulario.html', {"form": form})
+
+def actualizar(request, id):
+    bicicleta = get_object_or_404(Bicicleta, id=id) #Obtiene el objeto, en caso de no existir regresa un error 404
+    form = BicicletaForm(instance=bicicleta)
+    if request.method == 'POST':
+        form = BicicletaForm(request.POST, instance=bicicleta)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'formulario.html', {"form": form})
+
+def eliminar(request, id):
+    bicicleta = get_object_or_404(Bicicleta, id=id)
+    bicicleta.delete()
+    return redirect('home')
+
+def detalle(request, id):
+    bicicleta = get_object_or_404(Bicicleta, id=id)
+    return render(request, 'detalle.html', {"bicicleta": bic})
