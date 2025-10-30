691d3da32cdf57b8402f4954fae2a9d3a2827d06
Cynthia Castillo <ca.castilloe@gmail.com>
2025-10-30 03:00:27 +0300

    CRUD con CBV

--- Módulo 7/proyecto_mascotas/mascotas/urls.py
+++ Módulo 7/proyecto_mascotas/mascotas/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views #Importar todo
-#from .views import MascotaListView, MascotaDetailView
+from .views import *
 
 urlpatterns = [
-    path('', MascotaListView.as_view(), name='lista_mascotas')
+    path('', MascotaListView.as_view(), name='lista_mascotas'),
+    path('nueva/', MascotaCreateView.as_view(), name='crear_mascota'),
+    path('editar/<int:pk>/', MascotaUpdateView.as_view(), name='editar_mascota'),
+    path('borrar/<int:pk>/', MascotaDeleteView.as_view(), name='borrar_mascota'),
+    path('detalle/<int:pk>/', MascotaDetailView.as_view(), name='detalle_mascota'),
 ]
--- Módulo 7/proyecto_mascotas/mascotas/views.py
+++ Módulo 7/proyecto_mascotas/mascotas/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from .models import Mascota
+from .forms import MascotaForm
+from django.urls import reverse_lazy
 
 class MascotaListView(ListView):
     model = Mascota
@@ -8,3 +10,27 @@ class MascotaListView(ListView):
     template_name = "lista_mascotas.html"
     #envia a la plantilla modelo_list -> mascota_list
     context_object_name = "mascotas"
+
+class MascotaCreateView(CreateView):
+    model = Mascota
+    form_class = MascotaForm
+    template_name = "formulario_mascota.html" #por defecto: templates/mascotas/mascota_form.html
+    success_url = reverse_lazy("lista_mascotas") #Devolver la URL correspondiente a ese nombre
+
+class MascotaUpdateView(UpdateView):
+    model = Mascota
+    #form_class = MascotaForm
+    fields = ['nombre', 'especie', 'edad', 'descripcion']
+    template_name = "formulario_mascota.html" #por defecto: templates/mascotas/mascota_form.html
+    success_url = reverse_lazy("lista_mascotas") #Devolver la URL correspondiente a ese nombre
+
+class MascotaDeleteView(DeleteView):
+    model = Mascota
+    #por defecto: templates/mascotas/mascota_confirm_delete.html
+    template_name = "confirmar_borrado_mascota.html"
+    success_url = reverse_lazy("lista_mascotas")
+
+class MascotaDetailView(DetailView):
+    model = Mascota
+    template_name = "detalle_mascota.html" #por defecto: templates/mascotas/mascota_detail.html
+    context_object_name = "mascota" #object
